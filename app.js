7ec84041a5d46ca3799b2ab45e1003961d3024d0
misha <lolipopp345@gmail.com>
2023-03-20 23:52:34 +0300

    Update

---
+++ app.js
@@ -0,0 +1,20 @@
+VANTA.BIRDS({
+    el: "#selector",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    backgroundColor: 0x0,
+    color1: 0xa0066,
+    color2: 0xffffff,
+    colorMode: "variance",
+    birdSize: 0.20,
+    wingSpan: 10.00,
+    speedLimit: 1.00,
+    separation: 100.00,
+    alignment: 1.00,
+    cohesion: 1.00
+})
