3f8f25e39a64b755da71f1b75fb899f268db27e4
misha <lolipopp345@gmail.com>
2023-03-23 17:24:42 +0300

    Update3

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ VANTA.BIRDS({
     cohesion: 1.00
 })
 
-//////////////////////////////////////////////
+////////////////////////////////////////////// смена картинки для каждого отдельного NFT ----------->
 let imagesOne  = ['img/1.jpg', 'img/2.jpg',]
 let lengthOne = imagesOne.length;
 let indexOne = 1;
@@ -101,3 +101,33 @@ setInterval(function() {
     document.getElementById('nine',).src = imagesNine[indexNine++];
 }, 30000);
 //////////////////////////////////////////////
+
+AOS.init({
+    disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+    startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+    initClassName: 'aos-init', // class applied after initialization
+    animatedClassName: 'aos-animate', // class applied on animation
+    useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+    disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+    debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+    throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+
+
+    // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+    offset: 120, // offset (in px) from the original trigger point
+    delay: 0, // values from 0 to 3000, with step 50ms
+    duration: 1000, // values from 0 to 3000, with step 50ms
+    easing: 'ease', // default easing for AOS animations
+    once: false, // whether animation should happen only once - while scrolling down
+    mirror: false, // whether elements should animate out while scrolling past them
+    anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+})
+
+//// подключение метамаск
+async function connect() {
+    if (window.ethereum) {
+        await window.ethereum.request ({method: "eth_requestAccounts"});
+        window.web3 = new Web3 (window.ethereum);
+        document.getElementById ("MM").value = "Mint";
+    }
+}
